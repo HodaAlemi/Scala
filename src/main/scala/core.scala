8411e5feac15045f587402782405362a6403a173
Alemi Neissi <XU45SR@europe.intranet>
2017-03-17 11:35:51 +0100

    index.html is added with a dom window alert

--- src/main/scala/core.scala
+++ src/main/scala/core.scala
@@ -1,6 +1,10 @@
 import scala.scalajs.js
+import org.scalajs.dom
+import dom.document
 
 object core extends js.JSApp{
   @scala.scalajs.js.annotation.JSExport
-  override def main(): Unit = ???
+  def main(): Unit = {
+    dom.window.alert("Hi from Scala-js-dom")
+  }
 }
