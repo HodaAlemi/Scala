c80f4be21c9f7b932b1e7b97392643453d3090c8
Hoda Alemi <hoda.alemi@ing.nl>
2017-03-31 12:43:13 +0200

    adding encryption utility gui: WIP

--- src/main/scala/core.scala
+++ src/main/scala/core.scala
@@ -1,25 +1,15 @@
 import scala.scalajs.js
-import org.scalajs.dom
-import dom.document
-
+import org.scalajs.jquery.jQuery
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 
 object core extends js.JSApp{
   @scala.scalajs.js.annotation.JSExport
   def main(): Unit = {
-
-  }
-
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
   }
 
   @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    jQuery("body").append("<p>You clicked the curl button!</p>")
   }
 }
